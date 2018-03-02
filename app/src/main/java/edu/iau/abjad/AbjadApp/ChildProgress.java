f7cc5b3a81ba62cb6fe9c81c3157ce02c02c2978
lobnasyr <lobna.alzeer@gmail.com>
2018-03-02 17:34:14 +0300

    Adding one java class of norah work

--- app/src/main/java/edu/iau/abjad/AbjadApp/ChildProgress.java
+++ app/src/main/java/edu/iau/abjad/AbjadApp/ChildProgress.java
@@ -1,13 +1,28 @@
 package edu.iau.abjad.AbjadApp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
-public class ChildProgress extends AppCompatActivity {
+public class ChildProgress extends menu_educator {
+    menu_variables m = new menu_variables();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_child_progress);
+
+        m.title = (TextView) findViewById(R.id.interface_title);
+        m.title.setText("أحمد عمر");
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        //inflate your activity layout here!
+        View contentView = inflater.inflate(R.layout.activity_child_progress, null, false);
+
+        mDrawerLayout.addView(contentView, 0);
+
     }
 }
