b573ba3e445e3c49e0fdc8722716fd4f6d0ac5e7
Marwahalattas <marwahalattas@live.com>
2018-03-04 19:54:15 +0300

    Educator sign in is added Margins in text fields are set "next" button in sign up is edited
    :)

--- app/src/main/java/edu/iau/abjad/AbjadApp/SigninEducator.java
+++ app/src/main/java/edu/iau/abjad/AbjadApp/SigninEducator.java
@@ -3,11 +3,11 @@ package edu.iau.abjad.AbjadApp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Signin extends AppCompatActivity {
+public class SigninEducator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin_child);
+        setContentView(R.layout.activity_signin_educator);
     }
 }
