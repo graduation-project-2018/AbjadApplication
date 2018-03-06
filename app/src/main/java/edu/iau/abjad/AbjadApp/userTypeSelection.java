8904761772b15e4d8e2cc715674c7cf121868269
alaaH <ala2.hpapa@gmail.com>
2018-03-06 16:58:15 +0300

    User Type selection Done

--- app/src/main/java/edu/iau/abjad/AbjadApp/userTypeSelection.java
+++ app/src/main/java/edu/iau/abjad/AbjadApp/userTypeSelection.java
@@ -1,7 +1,11 @@
 package edu.iau.abjad.AbjadApp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class userTypeSelection extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class userTypeSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_type_selection);
+        Intent intentOfChild=new Intent(this, Signin.class );
+        Intent intentOfEdu=new Intent(this, SigninEducator.class );
+
+        ImageView ChildIcon = (ImageView) findViewById(R.id.child_btn);
+        ImageView EduIcon = (ImageView) findViewById(R.id.educator_Btn);
+
+        if(ChildIcon.isClickable()){
+            startActivity(intentOfChild);
+        }
+        else if(EduIcon.isClickable()){
+            startActivity(intentOfEdu);
+
+        }
+
+
     }
 }
