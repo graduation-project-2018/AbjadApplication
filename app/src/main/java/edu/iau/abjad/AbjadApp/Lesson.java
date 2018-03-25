ef217a31f03e50cd1ec637d81da1ea010d1e2e01
lobnasyr <lobna.alzeer@gmail.com>
2018-03-25 18:55:22 +0300

    سماع صوت كلمات الدرس مع عرض الصور وسماع تعليمات بداية كل درس وإضافة فنكشن لتحقق من الألف مع همزة أو لا

--- app/src/main/java/edu/iau/abjad/AbjadApp/Lesson.java
+++ app/src/main/java/edu/iau/abjad/AbjadApp/Lesson.java
@@ -6,9 +6,12 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Build;
+import android.os.CountDownTimer;
 import android.provider.Settings;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -21,6 +24,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -28,25 +32,43 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Lesson extends child_menu {
+public class Lesson extends child_menu implements MediaPlayer.OnPreparedListener {
     menu_variables m = new menu_variables();
-    TextView result;
+    TextView word_label;
     Button mic_btn;
     SpeechRecognizer mSpeechRecognizer ;
     Intent mSpeechRecognizerIntent ;
-
+    Button next_lesson_btn;
+    int words_counter;
+    String word;
+    firebase_connection r;
+    ImageView lesson_pic;
     String lessonID;
+    ArrayList <lesson_words> wordsArrayList = new ArrayList<lesson_words>();
+    ArrayList <lesson_sentences> sentencesArrayList = new ArrayList<lesson_sentences>();
+    MediaPlayer lesson_audio = new MediaPlayer();
+    MediaPlayer lesson_begin_audio = new MediaPlayer();
+    Button speaker_btn;
+    private  static CountDownTimer countDownTimer;
+    boolean flag = true;
+
+
+
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         m.title = (TextView) findViewById(R.id.interface_title);
-        m.title.setText("الدرس الأول");
+        m.title.setText("حرف ( م )");
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         //inflate your activity layout here!
@@ -54,18 +76,54 @@ public class Lesson extends child_menu {
 
         myDrawerLayout.addView(contentView, 0);
 
-        final firebase_connection r = new firebase_connection();
 
 
-       /* r.ref.child("Lessons").push().child("sentences").child("sentence1").
-                child("content").setValue("زجاج النافذة نظيف");
-        r.ref.child("Lessons").child("lesson1").child("sentences").child("sentence1").
-                child("audio_file").setValue("clean_window");
-        r.ref.child("Lessons").child("lesson1").child("sentences").child("sentence1").
-                child("pic_file").setValue("clean_window");
-        r.ref.child("Lessons").child("unitID").setValue("unit1");*/
 
-       //getting the lesson ID for the selected letter in Unit interface.
+        r = new firebase_connection();
+        next_lesson_btn = (Button) findViewById(R.id.next_lesson);
+        word_label = (TextView) findViewById(R.id.word_label);
+        lesson_pic = (ImageView) findViewById(R.id.lesson_pic);
+        words_counter =0;
+        speaker_btn = (Button) findViewById(R.id.speaker_btn);
+
+
+        /*r.ref.child("Lessons").child("lesson3").child("lesson_letter").setValue("ل");
+        r.ref.child("Lessons").child("lesson3").child("unitID").setValue("unit1");
+
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word1").child("audio_file").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word1").child("content").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word1").child("pic_file").setValue("-");
+
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word2").child("audio_file").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word2").child("content").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word2").child("pic_file").setValue("-");
+
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word3").child("audio_file").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word3").child("content").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word3").child("pic_file").setValue("-");
+
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word4").child("audio_file").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word4").child("content").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("Words").child("word4").child("pic_file").setValue("-");
+
+
+        r.ref.child("Lessons").child("lesson3").child("sentences").child("sentence1").child("audio_file").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("sentences").child("sentence1").child("content").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("sentences").child("sentence1").child("pic_file").setValue("-");
+
+        r.ref.child("Lessons").child("lesson3").child("sentences").child("sentence2").child("audio_file").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("sentences").child("sentence2").child("content").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("sentences").child("sentence2").child("pic_file").setValue("-");
+
+        r.ref.child("Lessons").child("lesson3").child("sentences").child("sentence3").child("audio_file").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("sentences").child("sentence3").child("content").setValue("-");
+        r.ref.child("Lessons").child("lesson3").child("sentences").child("sentence3").child("pic_file").setValue("-");*/
+
+
+
+
+
+        //getting the lesson ID of the selected letter in Unit interface.
         Query query = r.ref.child("Lessons").orderByChild("lesson_letter").equalTo("م");
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -75,6 +133,94 @@ public class Lesson extends child_menu {
 
                     for (DataSnapshot letter : dataSnapshot.getChildren()) {
                          lessonID = letter.getKey();
+                        Log.d( "1","Lesson ID: "+ lessonID);
+
+                    }
+                    if(lessonID != null){
+
+                        DatabaseReference read = r.ref.child("Lessons").child(lessonID).child("Words");
+
+                        read.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                for (final DataSnapshot word_ls : dataSnapshot.getChildren()){
+
+                                    String content = word_ls.child("content").getValue().toString();
+                                    String audio = word_ls.child("audio_file").getValue().toString();
+                                    String pic = word_ls.child("pic_file").getValue().toString();
+
+                                    lesson_words obj = new lesson_words(content, audio, pic);
+                                    wordsArrayList.add(obj);
+                                    word = wordsArrayList.get(words_counter).content;
+
+                                    word_label.setText(word);
+                                    check_alef();
+                                    Picasso.get().load(wordsArrayList.get(words_counter).pic_file).into(lesson_pic);
+                                    String lesson_begin_url = "https://firebasestorage.googleapis.com/v0/b/abjad-a0f5e.appspot.com/o/%D9%88%D8%A7%D8%AC%D9%87%D8%A9%20%D8%A7%D9%84%D8%AF%D8%B1%D8%B3%20(%D8%A3%D9%88%D9%84%20%D9%86%D8%B5%D9%8A%D8%AD%D8%A9%20%D8%8C%20%D8%A8%D8%AF%D8%A7%D9%8A%D8%A9%20%D9%83%D9%84%20%D8%AF%D8%B1%D8%B3).mp3?alt=media&token=dc8cc360-de38-4eab-96b6-256a602b2f86";
+
+                                   /*countDownTimer= new CountDownTimer(10000, 1000) {
+
+                                        public void onTick(long millisUntilFinished) {
+                                            System.out.println("onTick methed");
+                                        }
+
+                                        public void onFinish() {
+
+                                            System.out.println("onFinish methed");
+                                            playAudio(wordsArrayList.get(words_counter).audio_file);
+                                        }
+                                    }.start();*/
+
+                                    // start the instruction audio before the lesson begin
+                                    playAudio(lesson_begin_url);
+
+
+                                    // On complete listener that fire when the instruction audio finish to start the lesson audio.
+                                    lesson_audio.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                                        @Override
+                                        public void onCompletion(MediaPlayer mediaPlayer) {
+                                            if(flag == false){
+                                                return;
+                                            }
+                                            flag = false;
+                                            playAudio(wordsArrayList.get(words_counter).audio_file);
+
+                                        }
+                                    });
+
+                                    speaker_btn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+
+
+                                            lesson_audio.start();
+                                        }
+                                    });
+
+                                    next_lesson_btn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            words_counter++;
+                                            if(words_counter == 3){
+                                                next_lesson_btn.setVisibility(View.INVISIBLE);
+                                            }
+                                            word = wordsArrayList.get(words_counter).content;
+                                            word_label.setText(word);
+                                            check_alef();
+                                            Picasso.get().load(wordsArrayList.get(words_counter).pic_file).into(lesson_pic);
+                                            playAudio(wordsArrayList.get(words_counter).audio_file);
+                                        }
+                                    });
+
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError error) {
+                                // Failed to read value
+                                Log.w(null, "Failed to read value.", error.toException());
+                            }
+                        });
                     }
                 }
                 else{
@@ -87,37 +233,7 @@ public class Lesson extends child_menu {
             }
         });
 
-        DatabaseReference read = r.ref.child("Lessons").child(lessonID).child("Words");
-
-        read.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot word : dataSnapshot.getChildren()){
-
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(null, "Failed to read value.", error.toException());
-            }
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        result = (TextView) findViewById(R.id.word_label);
+      //******* Starting speech recognition code ********
         mic_btn = (Button) findViewById(R.id.mic_btn);
 
         checkPermission();
@@ -172,10 +288,13 @@ public class Lesson extends child_menu {
 
                     for(int i =0 ; i< matches.size(); i++){
                         Log.d("2", "Results " + matches.get(i));
+                        if(matches.get(i).compareTo(word)== 0){
+                            Log.d("2", "Matching true!! ");
+                            break;
+                        }
                     }
-
                     if(matches != null){
-                        result.setText(matches.get(0));
+                        //word_label.setText(matches.get(0));
                     }
             }
 
@@ -194,12 +313,10 @@ public class Lesson extends child_menu {
                 switch(motionEvent.getAction()){
                     case MotionEvent.ACTION_UP:{ //user release his finger
                       mSpeechRecognizer.stopListening();
-                      result.setText("You will see result here");
+
                         break;
                     }
                     case MotionEvent.ACTION_DOWN:{ //user press the mic button
-                        result.setText("");
-                        result.setText("Listening...");
                         mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
                         break;
                     }
@@ -222,4 +339,57 @@ public class Lesson extends child_menu {
             }
         }
     }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+
+    }
+
+    public void playAudio(String url){
+        try {
+
+            lesson_audio.reset();
+            lesson_audio.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            lesson_audio.setDataSource(url);
+            lesson_audio.prepare();
+            lesson_audio.start();
+
+        }
+        catch (IOException e){
+            Log.d("5","inside IOException ");
+        }
+
+        catch (IllegalArgumentException e){
+            Log.d("5"," inside IllegalArgumentException");
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+            Log.d("5","Inside exception");
+        }
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        lesson_audio.release();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        lesson_audio.release();
+    }
+
+    public void check_alef(){
+        if(word.indexOf('أ')!= -1){
+
+            word = word.replace('أ','ا');
+            Log.d("5","Word is: "+ word);
+        }
+
+    }
+
 }
--- app/src/main/java/edu/iau/abjad/AbjadApp/educator_home.java
+++ app/src/main/java/edu/iau/abjad/AbjadApp/educator_home.java
@@ -43,6 +43,17 @@ public class educator_home extends menu_educator {
         final Button btn = (Button) findViewById(R.id.add_new_child_btn);
         final firebase_connection r = new firebase_connection();
         final DatabaseReference read = r.ref.child("Children").child("childID").child("first_name");
+        r.ref.child("Units").child("unit2").child("unit letters").child("L1").setValue("ص");
+        r.ref.child("Units").child("unit2").child("unit letters").child("L2").setValue("ف");
+        r.ref.child("Units").child("unit2").child("unit letters").child("L3").setValue("س");
+        r.ref.child("Units").child("unit2").child("unit letters").child("L4").setValue("ق");
+        r.ref.child("Units").child("unit2").child("unit letters").child("L5").setValue("ت");
+        r.ref.child("Units").child("unit2").child("unit letters").child("L6").setValue("ح");
+
+        r.ref.child("Units").child("unit2").child("unit pic").setValue("-");
+        r.ref.child("Units").child("unit2").child("unit title").setValue("مدرستي");
+
+
 
         read.addValueEventListener(new ValueEventListener() {
             @Override
