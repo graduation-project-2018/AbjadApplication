87142b86762e6af03955877606d9297978484171
alaaH <ala2.hpapa@gmail.com>
2018-03-29 02:06:05 +0300

    Update on Report problem : 1- add access permision dialog 2- problem type selection (add to DB and handle error if not selected) 3-select photo and upload to storage , still I have som problem in adding right URL to DB.

--- app/src/main/java/edu/iau/abjad/AbjadApp/report_problem.java
+++ app/src/main/java/edu/iau/abjad/AbjadApp/report_problem.java
@@ -1,19 +1,29 @@
 package edu.iau.abjad.AbjadApp;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -51,12 +61,15 @@ public class report_problem extends menu_educator {
     private String dis;
     private EditText moreDetails;
     private HashMap<String,String> dataMap;
-
+    private Uri imgURI;
+    private ProgressBar loding;
+    private boolean uploded=false;
+    private int storagePermCode=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        m.title = (TextView) findViewById(R.id.interface_title);
+        m.title = findViewById(R.id.interface_title);
 
         m.title.setText("إبلاغ عن مشكلة");
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -67,10 +80,11 @@ public class report_problem extends menu_educator {
         //initialization
         uplodImgbtn = findViewById(R.id.uplodBtn);
         reportBtn= findViewById(R.id.reportButn );
-        ImgNameTextView=(TextView) findViewById(R.id.imgname);
+        ImgNameTextView= findViewById(R.id.imgname);
         rg= findViewById(R.id.radioButtonsGroup);
         dataMap=new HashMap<String, String>();
-
+        loding= findViewById(R.id.loading);
+        loding.setVisibility(View.GONE);
         moreDetails=findViewById(R.id.moredetails);
         //firbase storge refrance
         mStorge = FirebaseStorage.getInstance().getReference();
@@ -78,57 +92,96 @@ public class report_problem extends menu_educator {
         reportBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //uploading screenshot
-            System.out.println(ProbType);
-            System.out.println(dis);
+                loding.setVisibility(View.VISIBLE);
+                final StorageReference filePath = mStorge.child("ReportProblemScreenshots").child(imgURI.getLastPathSegment());
+                    StorageTask<UploadTask.TaskSnapshot> taskSnapshotStorageTask = filePath.putFile(imgURI).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(final UploadTask.TaskSnapshot taskSnapshot) {
 
-                dataMap.put("report_type",ProbType);
-                dataMap.put("report_description",dis);
-                dataMap.put("reporter_email","ala2.hpapa@gmail.com");
-                report.ref.child("Reports").push().setValue(dataMap);
+                            filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    Uri downloadUri=uri;
+                                    daownloadURL=downloadUri.toString();
+                                }
+                            });
+                            dataMap.put("screenShots",daownloadURL);
+                            uploded=true;
+                            loding.setVisibility(View.GONE);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(report_problem.this, "خطأ في رفع الصورة", Toast.LENGTH_LONG).show();
+                            System.out.println(e);
+                        }
+                    });
+
+                //uploading screenshot
+
+                if(rg.getCheckedRadioButtonId()!=-1 && uploded){
+                    dataMap.put("report_type",ProbType);
+                    dataMap.put("report_description",dis);
+                    dataMap.put("reporter_email","ala2.hpapa@gmail.com");
+                    report.ref.child("Reports").push().setValue(dataMap);
+                    Toast.makeText(report_problem.this,"شكرا، تم تلقي البلاغ!",Toast.LENGTH_LONG).show();
+                    rg.clearCheck();
+                    ImgNameTextView.setText("");
+                    moreDetails.setText("");
+                    //daownloadURL=null;
+                }
+                else if(rg.getCheckedRadioButtonId()==-1){
+                    Toast.makeText(report_problem.this,"الرجاء تحديد نوع المشكلة",Toast.LENGTH_LONG).show();
+                }
+                else if(!uploded){
+                    Toast.makeText(report_problem.this," يتم رفع الصورة",Toast.LENGTH_LONG).show();
+
+                }
 
             }
         });
     }
 
     public void onImgGalleryCliked(View view) {
-        Intent picChooserIntent = new Intent(Intent.ACTION_PICK);
-        File picDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        String picDirPath = picDirectory.getPath();
-        Uri data = Uri.parse(picDirPath);
-        picChooserIntent.setDataAndType(data, "image/*");
-        startActivityForResult(picChooserIntent, PIC_GALLERY_REQUIST);
+        if (ContextCompat.checkSelfPermission(report_problem.this, Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+            Intent picChooserIntent = new Intent(Intent.ACTION_PICK);
+            File picDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            String picDirPath = picDirectory.getPath();
+            Uri data = Uri.parse(picDirPath);
+            picChooserIntent.setDataAndType(data, "image/*");
+            startActivityForResult(picChooserIntent, PIC_GALLERY_REQUIST);
+        }
+        else{
+            requestStoragePermission();
+        }
+
 
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
         if (resultCode == RESULT_OK) {
             if (requestCode == PIC_GALLERY_REQUIST) {
-                Uri imgURI  = data.getData();
-                final StorageReference filePath = mStorge.child("ReportProblemScreenshots").child(imgURI.getLastPathSegment());
-                StorageTask<UploadTask.TaskSnapshot> taskSnapshotStorageTask = filePath.putFile(imgURI).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(final UploadTask.TaskSnapshot taskSnapshot) {
-                        Toast.makeText(report_problem.this, "تم رفع الصورة", Toast.LENGTH_LONG).show();
-                        filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                            @Override
-                            public void onSuccess(Uri uri) {
-                                Uri downloadUri=uri;
-                                daownloadURL=downloadUri.toString();
-                            }
-                        });
-                        ImgNameTextView.setText(filePath.getName());
-                        report.ref.child("Reports").push().child("screenShots").setValue(daownloadURL);
-
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(report_problem.this, "خطأ في رفع الصورة", Toast.LENGTH_LONG).show();
-                    }
-                });
+                imgURI = data.getData();
+                String imgNAmeS,schema;
+                schema=imgURI.getScheme();
+                if(schema.equals("file")){imgNAmeS=imgURI.getLastPathSegment();}
+                else if(schema.equals("content")){String [] i={MediaStore.Images.Media.TITLE};
+                Cursor c= getContentResolver().query(imgURI,i,null,null,null);
+                if(c!=null && c.getCount()!=0){ int colindx=c.getColumnIndexOrThrow(MediaStore.Images.Media.TITLE);
+                c.moveToFirst();
+                imgNAmeS=c.getString(colindx);
+                ImgNameTextView.setText(imgNAmeS);}
+                if(c!=null){
+                    c.close();
+                }
+                }
             }
+            else{
+                Toast.makeText(report_problem.this,"No img selected",Toast.LENGTH_LONG ).show();
+            }
+
         }
+
 
     }
 
@@ -137,13 +190,52 @@ public class report_problem extends menu_educator {
         problemType=findViewById(radioId);
         ProbType=(String) problemType.getText();
         RadioButton more=findViewById(R.id.more);
+        moreDetails.setHint("الوصف");
         if(more.isChecked()){
             moreDetails.setEnabled(true);
-            dis=moreDetails.getText().toString();
+            moreDetails.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    dis=moreDetails.getText().toString();
+                    return false;
+                }
+            });
         } else { moreDetails.setEnabled(false);}
 
 
     }
+
+private  void requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            new AlertDialog.Builder(this).setTitle("بحاجة للوصول").setMessage("بحاجة للوصول الى الصور").setPositiveButton("موافق", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    ActivityCompat.requestPermissions(report_problem.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, storagePermCode);
+
+                }
+            }).setNegativeButton("الغاء", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).create().show();
+        }else{
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, storagePermCode);
+        }
+}
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+      if(requestCode== storagePermCode){
+          if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+              Toast.makeText(this,"تم الوصول", Toast.LENGTH_SHORT).show();
+          }else{
+              Toast.makeText(this,"لم يتم الوصول الى الصور",Toast.LENGTH_SHORT);
+          }
+      }
+    }
+
 
 
 }
