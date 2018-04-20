cc5507bcea11239e959b0772e2830d98753dd65e
Marwah <marwahalattas@live.com>
2018-04-20 14:10:43 +0300

    :)

--- app/src/main/java/edu/iau/abjad/AbjadApp/change_profile_photo.java
+++ app/src/main/java/edu/iau/abjad/AbjadApp/change_profile_photo.java
@@ -5,10 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class change_profile_photo extends child_menu {
     menu_variables m = new menu_variables();
+    private ImageView childImg;
+    private ImageView next;
+    private ImageView pre;
+    private ArrayList<String> imgsUri;
+    private firebase_connection FBchildPhotoUrl;
+    private long imgCont;
+    private int imgIndex;
+    String photo_url;
+    Button SaveChanges;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +41,85 @@ public class change_profile_photo extends child_menu {
         View contentView = inflater.inflate(R.layout.activity_change_profile_photo, null, false);
 
         myDrawerLayout.addView(contentView, 0);
+
+        next = (ImageView)findViewById(R.id.nextChangeImg);
+        pre = (ImageView)findViewById(R.id.prevChangeImg);
+        childImg=(ImageView)findViewById(R.id.ChangeChildImg);
+        FBchildPhotoUrl = new firebase_connection();
+        imgIndex = 0;
+        photo_url="";
+        SaveChanges=(Button)findViewById(R.id.SaveChangeImg);
+        FBchildPhotoUrl.ref.child("ChildPhoto").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot childSnapShot : dataSnapshot.getChildren()) {
+                    imgCont = childSnapShot.getChildrenCount();
+                    String photoUri = childSnapShot.getValue().toString();
+                    imgsUri.add(photoUri);
+
+
+                }
+                // load();
+
+                Picasso.get().load(imgsUri.get(0)).fit().centerInside().into(childImg);
+                photo_url = imgsUri.get(0);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(change_profile_photo.this, "Error", Toast.LENGTH_LONG).show();
+
+            }
+
+        });
+        //load();
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // load();
+                imgIndex++;
+                if (imgIndex < 6) {
+                    Picasso.get().load(imgsUri.get(imgIndex)).fit().centerInside().into(childImg);
+                    photo_url=imgsUri.get(imgIndex);
+                } else {
+                    imgIndex = 0;
+                    Picasso.get().load(imgsUri.get(imgIndex)).fit().centerInside().into(childImg);
+                    photo_url=imgsUri.get(imgIndex);
+
+                }
+
+            }
+        });
+        //load();
+        pre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // load();
+                imgIndex--;
+                if (imgIndex < 6 & imgIndex > 0) {
+                    Picasso.get().load(imgsUri.get(imgIndex)).fit().centerInside().into(childImg);
+                    photo_url=imgsUri.get(imgIndex);
+
+                } else if (imgIndex == -1) {
+                    imgIndex = 5;
+                    Picasso.get().load(imgsUri.get(imgIndex)).fit().centerInside().into(childImg);
+                    photo_url=imgsUri.get(imgIndex);
+
+                } else {
+                    imgIndex = 0;
+                    Picasso.get().load(imgsUri.get(imgIndex)).fit().centerInside().into(childImg);
+                    photo_url=imgsUri.get(imgIndex);
+
+                }
+            }
+        });
+
+        SaveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+        //load();
     }
 }
