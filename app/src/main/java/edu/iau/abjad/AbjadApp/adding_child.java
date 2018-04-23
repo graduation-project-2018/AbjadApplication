2deb736c4207b6e9765c33bbdeb7b01c495da899
Marwah <marwahalattas@live.com>
2018-04-23 08:50:22 +0300

    :)

--- app/src/main/java/edu/iau/abjad/AbjadApp/adding_child.java
+++ app/src/main/java/edu/iau/abjad/AbjadApp/adding_child.java
@@ -2,17 +2,13 @@ package edu.iau.abjad.AbjadApp;
 
 import android.support.v7.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
+
 import android.content.Intent;
 import android.os.Bundle;
-
-import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
